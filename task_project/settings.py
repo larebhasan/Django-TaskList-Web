b6b8a0cf48d575ccc20e08552d8a327b2866cb30
Lareb Hasan <143859866+larebhasan@users.noreply.github.com>
2026-01-23 03:11:30 +0530

    Dockerize Django app with Dockerfile and docker-compose

--- task_project/settings.py
+++ task_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!&aq2cv8o1ohs1txoaje$+9%)eii!7%)e2lhaodbb*q9nl&f&o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
